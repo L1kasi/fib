f6448d0a15a4b379d69b35119a5760d78e6d39e9
L1kasi <46110956+L1kasi@users.noreply.github.com>
2018-12-28 18:35:38 +0500

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    
+    class Fibonachi
+    {
+        public int Val1 { get; set; }
+        public int Val2 { get; set; }
+        public int N { get; set; }
+        static void Main(string[] args)
+        {
+            Fibonachi F = new Fibonachi();
+            F.Val2=F.Val1 = 1; int a;
+            F.N = int.Parse(Console.ReadLine());
+            if (F.N > 0)
+            {
+               for (int i=0; i<F.N;i++ )
+                {
+                    Console.WriteLine(F.Val1);
+                    a = F.Val2; F.Val2 = F.Val2 + F.Val1; F.Val1 = a;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
